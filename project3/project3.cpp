8fced90599781390409a41c72a24a448aa1a5c67
aalot001 <aalot001@FLLAB-19>
2017-04-12 11:27:52 -0400

    Hello GitHub

--- project3/project3.cpp
+++ project3/project3.cpp
@@ -5,7 +5,6 @@
 #include <iostream>
 using namespace std;
 //To reduce lines in the code, I am going to write every expression in a line.
-
 int main()
-{ cout << "Hello World..!" << endl; return 0; }
+{ cout << "Hello GitHub..!" << endl; return 0; }
 
