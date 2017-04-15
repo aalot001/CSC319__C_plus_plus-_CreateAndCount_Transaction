82d89c81bd05d6776a8955dc327e027caa15f661
aalot001 <aalot001@FLLAB-19>
2017-04-14 21:41:52 -0400

    Hello GitHub

--- project3/project3.cpp
+++ project3/project3.cpp
@@ -1,10 +1,10 @@
 // project3.cpp : Defines the entry point for the console application.
 
-//Ahmed Alotaibi. 4/12/2017
+//Ahmed Alotaibi. 4/12/2017 
 #include "stdafx.h"
 #include <iostream>
 using namespace std;
 //To reduce lines in the code, I am going to write every expression in a line.
 int main()
-{ cout << "Hello GitHub..!" << endl; return 0; }
+{ cout << "Hello GitHub.." << endl; return 0; }
 
