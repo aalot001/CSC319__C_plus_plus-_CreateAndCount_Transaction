e2b17c87b76c23cb6a5d7a060b1961020502f52d
aalot001 <aalot001@FLLAB-19>
2017-04-30 18:20:41 -0400

    Functions to read and write files;

--- project3/project3.cpp
+++ project3/project3.cpp
@@ -14,19 +14,67 @@ using std::endl;
 using std::ostream;
 using std::ifstream;
 
+//enum TerritoryType { NORMAL, PREMIUM }; // We need it: 
 
 struct Territory {
-	int territoryid;
+	int territoryid = 2;
 	long amount;
+	void read();
+
+	Territory() {
+		territoryid = 0;
+		amount = 0000;
+		if (territoryid == 0) {
+			cout << "I am zero: Territory struct: " << endl;
+		}
+	}
 };
 
+void Territory::read()
+{
+	cout << "Territory's Read function calls: " << endl;
+	string data;
+	ifstream territory("territory.txt");
+	ofstream output("out00.txt");
+	if (!territory.is_open()) {
+		cerr << "cannot open the file: territory.txt" << endl;
+	}
+	else {
+		cout << "Need to go over the file: Ready to read: " << endl;
+	}
+}
+
 struct Transaction {
-	int trxid;
+	int trxid = 5;
 	int salerepid;
 	int transactiontype;
 	long amount;
+	void read();
+
+	Transaction() {
+		trxid = 0;
+		if (trxid == 0) {
+			cout << "I am zero: Transaction struct: " << endl;
+		}
+	}
 };
 
+
+void Transaction::read()
+{
+	cout << "Transaction's read function calls: " << endl;
+	string data;
+	ifstream transaction;
+	transaction.open("transaction.txt");
+	ofstream output("out01.txt");
+
+	if (!transaction.is_open()) {
+		cerr << "cannot open the file: transaction.txt" << endl;
+	}
+	else {
+		cout << "Need to go over the file: Ready to read: " << endl;
+	}
+}
 struct Salerep {
 	int saleepid;
 	int territoryid;
@@ -37,8 +85,14 @@ int main(int argc, char *argv[])
 {
 	cout << argv << endl;
 	cout << argv[0] << endl;
-	cout << argv[1] << endl;
-	cout << argv[2] << endl;
-	
+	//cout << argv[1] << endl;
+	//cout << argv[2] << endl;
+	Territory test;
+	test.read();
+	Transaction test01;
+	test01.read();
+
 	return 0;
 }
+
+
