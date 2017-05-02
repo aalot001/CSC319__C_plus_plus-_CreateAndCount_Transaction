13af01bf6b8f878fc09c360f54ea513d3bea4387
aalot001 <aalot001@FLLAB-19>
2017-05-01 20:03:30 -0400

    namespace for conversion: naive loops;

--- project3/project3.cpp
+++ project3/project3.cpp
@@ -6,55 +6,59 @@
 #include <fstream>
 #include <ostream>
 #include <vector>
+#include <algorithm>
 
-using namespace std;
+// https://msdn.microsoft.com/en-us/library/5cb46ksf.aspx
+
+// Not sure which one of the following arugemnts are right to pass: 
+// <territory.txt>,<salerep.txt>,<transactions.txt>,<territory_output.txt> 
+// (territory.txt, salerep.txt, transactions.txt, territory_output.txt)
+
+//using namespace std; 
 
 using std::cout;
 using std::endl;
-using std::ostream;
+using std::vector;
+using std::ofstream;
 using std::ifstream;
+using std::string;
 
-enum TerritoryType { NORMAL, PREMIUM }; 
 
+enum TerritoryType { NORMAL, PREMIUM };
+
+
+namespace piece_casts {
+
+	int to_int(string s) {
+		int res = 0;
+		for (unsigned int i = 0; i < s.size(); i++) {
+			res = 10 * res + s[i] - '0';
+		}
+		return res;
+	}
+	long to_long(string s) {
+		long res = 0;
+		for (unsigned int i = 0; i < s.size(); i++) {
+			res = 10 * res + s[i] - '0';
+		}
+		return res;
+	}
+	TerritoryType to_ttype(string s) {
+		return s[0] == 'N' ? NORMAL : PREMIUM;
+	}
+}
 struct Territory {
 	int territoryid;
 	long amount;
 	TerritoryType type;
 
-	Territory(string _territoryid = 0, string _type = "");
-
-		TerritoryType to_ttype(string _type);
-		bool compare_territories(const Territory &a, const Territory &b);
+	Territory(string _territoryid, string _type) :
+		territoryid(piece_casts::to_int(_territoryid)),
+		type(piece_casts::to_ttype(_type)),
+		amount(0) {}
 };
 
-// Not sure ... 
-Territory::Territory(string _territoryid, string _type)
-{
-	ifstream territory_input("territory.txt");
-	string s;
-	vector<Territory> line;
-	if (!territory_input.is_open()) {
-		cerr << "Cannot open: territory.txt" << endl;
-	}
-	while (territory_input >> s) {
-		vector<string> p;
-		line.push_back(Territory(p[0], p[1]));
-	}
-	/*int res = 0;
-	for (unsigned int i = 0; i < line.size(); i++) {
-		res = 10 * res + s[i] - '0';
-		break;
-		cout << res << endl;
-	}*/
-}
-
-TerritoryType Territory::to_ttype(string _type)
-{
-	cout << _type << endl;
-	return _type[0] == 'N' ? NORMAL : PREMIUM;
-}
-
-bool Territory::compare_territories(const Territory & a, const Territory & b)
+bool compare_territories(const Territory & a, const Territory & b)
 {
 	return a.amount > b.amount;
 }
@@ -64,7 +68,6 @@ struct Transaction {
 	int salerepid;
 	int transactiontype;
 	long amount;
-	
 
 	Transaction() {}
 };
@@ -76,16 +79,23 @@ struct Salerep {
 	long amount;
 
 	Salerep() {}
+
 };
 
 int main(int argc, char *argv[])
 {
 	cout << argc << endl;
-	cout <<"Argv[0]: "<< argv[0] << endl;
+	//cout << "Argv[0]: " << argv[0] << endl;
 	cout <<"Argv[1]: "<< argv[1] << endl;
 	cout <<"Argv[2]: "<< argv[2] << endl;
 	cout << "Argv[3]: " << argv[3] << endl;
 	cout << "Argv[4]: " << argv[4] << endl;
-
+	string data;
+	ifstream territory_input(argv[1]);
+	vector<Territory> territories;
+	while (territory_input >> data) {
+		vector<string> p;
+		territories.push_back(Territory(p[0], p[1]));
+	  }
 	return 0;
 }
